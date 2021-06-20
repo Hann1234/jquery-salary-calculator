71f3da87fbdec8d178d43252d6037f486b67b1b8
Hann1234 <christopherjhannigan@gmail.com>
2021-06-20 13:30:24 -0500

    got button to add employee information to object

--- client.js
+++ client.js
@@ -2,13 +2,37 @@ console.log('JS'); //Make sure JS is sourced
 
 $(document).ready(onReady);
 
+const employeeArray = [];
+
+function addEmployee() {
+    const employee = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        employeeID: $('#employeeID').val(),
+        jobTitle: $('#jobTitle').val(),
+        annualSalary: $('#annualSalary').val()
+    };
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#employeeID').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
+
+    console.log('Employee added:', employee);
+}
+
 function onReady() {
     console.log('Page is loaded');
-    $('firstName').val('');
-    $('lastName').val('');
-    $('emplyeeID').val('');
-    $('jobTitle').val('');
-    $('firstName').val('');
-    $('annualSalary').val('');
-}
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#employeeID').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
+
+    let tot = $('#totalMonthly');
+    tot.empty();
+    tot.append(0);
+
+    $('#addEmployeeButton').on('click',addEmployee);
+  }
 
