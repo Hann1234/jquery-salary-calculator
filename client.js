1864ce4db40d32dad1ea755ff2f24e432a3a4737
Hann1234 <christopherjhannigan@gmail.com>
2021-06-19 22:31:59 -0500

    was going to make button in JS but decided to make it in HTML

--- client.js
+++ client.js
@@ -1,3 +1,14 @@
 console.log('JS'); //Make sure JS is sourced
 
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('Page is loaded');
+    $('firstName').val('');
+    $('lastName').val('');
+    $('emplyeeID').val('');
+    $('jobTitle').val('');
+    $('firstName').val('');
+    $('annualSalary').val('');
+}
 
