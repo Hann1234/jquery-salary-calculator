adc3ac68ac7a965cd612835b49d390d58710eb68
Hann1234 <christopherjhannigan@gmail.com>
2021-06-20 23:51:09 -0500

    finally got the delete button working correctly!

--- client.js
+++ client.js
@@ -50,19 +50,27 @@ function displayEmployees() { //function to display employees in the table
         <tr id="tableRow">
             <td id="firstNameTable">${person.firstName}</td>
             <td id="lastNameTable">${person.lastName}</td>
-            <td id="employeeIDTable">${person.employeeID}</td>
+            <td id="employeeID ${person.employeeID}">${person.employeeID}</td>
             <td id="jobTitleTable">${person.jobTitle}</td>
             <td id="annualSalaryTable">${person.annualSalary}</td>
             <td id="deleteButtonTable">
-                <button type="button" class="btn btn-danger btn-sm" id="deleteEmployeeButton">Delete</button>
+                <button type="button" class="btn btn-danger btn-sm" id="deleteEmployeeButton ${person.employeeID}">Delete</button>
             </td>
         </tr>
         `);
     }
 }
 
-function deleteEmployee() { //This removes the row but keeps the employee in the array...need to figure out how to remove the employee from the array then repopulate the table with the new array
-$(this).closest('tr').remove();
+//let color = $(this).attr('class').split(' ')[1]; // 'red' from color blocks
+
+function deleteEmployee() { //This removes the employee from the array then repopulates the table
+    let deleteID = $(this).attr('id').split(' ')[1]; //allows me to compare the unique employeeID being clicked for deletion to the employeeID numbers in the array
+    for (let i = 0; i < employeeArray.length; i++) {
+        if (employeeArray[i].employeeID === deleteID) {
+            employeeArray.splice(i,1); //YESSSSS FINALLY GOT IT TO WORK!!!! Deleting array items should be easier like idk array.delete[i]...CMON!
+        } 
+    }
+    displayEmployees();
 }
 
 function onReady() { //on page load clears input fields, sets value of employees to 0, and creates a listener for the button being clicked.
@@ -78,6 +86,6 @@ function onReady() { //on page load clears input fields, sets value of employees
     tot.append(0);
 
     $('#addEmployeeButton').on('click',addEmployee);
-    $('#table').on('click','#deleteEmployeeButton', deleteEmployee);
+    $('.table').on('click','.btn', deleteEmployee); //creates listener for delete button
   }
 
